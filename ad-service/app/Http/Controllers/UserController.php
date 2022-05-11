b7e00666b970b3b49887a8de3b28f9c94f3f1d5c
aodewanai <kuroshiro1iro@gmail.com>
2022-05-11 16:57:59 +0300

    auth and limit

---
+++ ad-service/app/Http/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    function login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response([
+                'message' => ['such user does not exist']
+            ], 404);
+        }
+
+        $token = $user->createToken('my-app-token')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+}
--- ad-service/routes/api.php
+++ ad-service/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('posts', [PostController::class, 'index']);
 
-Route::post('posts/create', [PostController::class, 'create']);
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('posts', [PostController::class, 'index']);
+
+    Route::post('posts/create', [PostController::class, 'create'])->middleware(['throttle:limit']);
+});
+
+
+Route::post("login", [UserController::class, 'login']);
